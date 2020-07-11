6723ede04d88fe5345a0025200b186924e74faf1
Banson Xu <xzcxzcyy@126.com>
2020-07-11 10:37:22 +0800

    feat&fix: sort; navigation.

--- app/src/main/java/cloud/banson/orangeNote/list/ListFragment.kt
+++ app/src/main/java/cloud/banson/orangeNote/list/ListFragment.kt
@@ -10,10 +10,21 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import cloud.banson.orangeNote.R
+import cloud.banson.orangeNote.database.Note
 import cloud.banson.orangeNote.database.NoteDatabase
 import cloud.banson.orangeNote.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
+    companion object {
+        private const val sortByTitle = 0
+        private const val sortByTitleDescend = 1
+        private const val sortByTime = 2
+        private const val sortByTimeDescend = 3
+    }
+
+    private var noteList: List<Note>? = null
+    private var sortOption = sortByTimeDescend
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -50,12 +61,28 @@ class ListFragment : Fragment() {
         })
         binding.noteList.adapter = adapter
 
-        viewModel.noteBook.observe(viewLifecycleOwner, Observer {
-            it?.let {
-                adapter.submitList(it)
-            }
+        viewModel.noteBook.observe(viewLifecycleOwner, Observer { newList ->
+            noteList = newList
+            updateNow(noteList, adapter)
         })
 
         return binding.root
     }
+
+    private fun updateNow(newList: List<Note>?, adapter: NoteAdapter) {
+        newList?.let { thisList ->
+            adapter.submitList(thisList.getSorted())
+        }
+    }
+
+    private fun List<Note>.getSorted(): List<Note>? {
+        return when (sortOption) {
+            sortByTitle -> this.sortedBy { note -> note.title }
+            sortByTitleDescend -> this.sortedByDescending { note -> note.title }
+            sortByTime -> this.sortedBy { note -> note.time }
+            sortByTimeDescend -> this.sortedByDescending { note -> note.time }
+            else -> null
+        }
+    }
+
 }
